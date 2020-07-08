d1b1d94c3070c4a918f0d19f2dc5e9ba4d086bd1
Bhavan Patel <bhavan.patel775@gmail.com>
2020-07-08 16:42:36 -0700

    Upload the account.java class

---
+++ Account.java
@@ -0,0 +1,53 @@
+
+public class Account {
+	private String nameString;
+	private String idString;
+	private String userNameString;
+	private String emailString;
+	private String passwordString;
+	private String securityQuetionString;
+	public String getNameString() {
+		return nameString;
+	}
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+	public String getIdString() {
+		return idString;
+	}
+	public void setIdString(String idString) {
+		this.idString = idString;
+	}
+	public String getUserNameString() {
+		return userNameString;
+	}
+	public void setUserNameString(String userNameString) {
+		this.userNameString = userNameString;
+	}
+	public String getEmailString() {
+		return emailString;
+	}
+	public void setEmailString(String emailString) {
+		this.emailString = emailString;
+	}
+	public String getPasswordString() {
+		return passwordString;
+	}
+	public void setPasswordString(String passwordString) {
+		this.passwordString = passwordString;
+	}
+	public String getSecurityQuetionString() {
+		return securityQuetionString;
+	}
+	public void setSecurityQuetionString(String securityQuetionString) {
+		this.securityQuetionString = securityQuetionString;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
